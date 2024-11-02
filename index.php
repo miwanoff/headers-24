826d81d1fa0187d17eab13952a2d62fc4846ecc2
miwanoff <marina.iva.noffa@gmail.com>
2024-11-02 13:35:44 +0200

    Add a

--- index.php
+++ index.php
@@ -7,11 +7,14 @@ ob_start();
 
 // header("HTTP/1.0 404 Not Found");
 // header("Location: http://www.example.com") ;
+// $user = "John";
+// header("Location:script.php?name=".$user);
 // header("HTTP/1.1 301 Moved Pemanently") ;
 // header("Refresh:5; url=http://www.example.com");
 echo "<h1>Hello</h1>";
 header("Content-Type: text/html; charset=utf-8");
 header("X-my-header: Hello world !") ;
 echo "Header";
+echo '<a href="script.php?name=Joan&date=2000-03-11">User</a>'; 
 // Відправляемо вміст буфера кліенту
 ob_end_flush();
