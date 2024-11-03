c9ef8b09f5b324c75c4f3abd31c72e3d99b0ca73
miwanoff <marina.iva.noffa@gmail.com>
2024-11-03 12:55:42 +0200

    Add select

--- test.php
+++ test.php
@@ -12,21 +12,34 @@
 if (isset($_POST['submit'])) {
     print_r($_POST);
     for ($i = 0; $i < count($_POST['in']); $i++) {
-        echo $_POST['in'][$i] . '<br/>';    }
-        $result_answer = $_POST['answer'];
-        if ($_POST['answer'] === "yes")
-           $result_answer = "Так!";
-        if ($_POST['answer'] === "no")
-           $result_answer = "Ні!";
-        echo "<p><b>".$result_answer."</b></p>";
+        echo $_POST['in'][$i] . '<br/>';
+    }
+    $result_answer = $_POST['answer'];
+    if ($_POST['answer'] === "yes") {
+        $result_answer = "Так!";
+    }
+
+    if ($_POST['answer'] === "no") {
+        $result_answer = "Ні!";
+    }
+
+    echo "<p><b>" . $result_answer . "</b></p>";
+    $course = $_POST['course'];
+    echo $course;
 }
 ?>
     <form method="post" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
-        <input type="text" name="in[]" value="1">
-        <input type="text" name="in[]" value="2">
-        <input type="text" name="in[]" value="3">
+        <input type="text" name="in[]" value="1"><br />
+        <input type="text" name="in[]" value="2"><br />
+        <input type="text" name="in[]" value="3"><br />
         <input type="radio" name="answer" value="yes" checked>yes<br />
         <input type="radio" name="answer" value="no">no<br />
+        <select name="course" size="1">
+            <option value="ASP.NET">ASP.NET</option>
+            <option value="PHP">PHP</option>
+            <option value="Ruby">RUBY</option>
+            <option value="Python">Python</option>
+        </select><br />
         <input type="submit" name="submit" value="Submit Data" />
     </form>
 </body>
